870045100f87d8a9deff8b5faa29646da3936aa0
Anthony-cloud-1 <tonysack13@gmail.com>
2024-02-08 16:28:12 -0800

    air

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a User."""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Represents a user."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
